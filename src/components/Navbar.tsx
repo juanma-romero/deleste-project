a2b5a839feea18510732b49b661f4fab15f1773d
Juanma Romero <xjuanmax@hotmail.com>
2023-05-30 01:13:42 -0400

    cambio selector idioma

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,80 +1,51 @@
 'use client'
 
-import { useState, Fragment } from 'react'
-import { Listbox, Transition } from '@headlessui/react'
-import {BsChevronContract,BsCheckLg} from 'react-icons/bs'
 import { GoThreeBars } from 'react-icons/go'
 import { Montserrat_Alternates } from 'next/font/google'
 import Link from 'next/link'
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { useState } from 'react'
+import { AR, BR } from 'country-flag-icons/react/1x1';
 
 const montserrat = Montserrat_Alternates({ subsets: ['latin'],weight: ['700'] })
 
-const idioma = [
-  { name: 'Idioma Brazil' },
-  { name: 'Idioma Argentina' }  
-]
+const Navbar = () => {  
+  const [age, setAge] = useState('');
 
-const Navbar = () => {
-  const [selected, setSelected] = useState(idioma[0])
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
+  
   return (
-    <div className=''>
-    <div className='flex justify-between items-center pt-6 px-6'>
-      
-      <h1 className={montserrat.className}><Link href='/'>Deleste.Shop</Link></h1>
-      <GoThreeBars className='cursor-pointer text-5xl rounded-lg py-2 px-3 shadow-md hover:bg-[#517373]'/>
-    </div> 
-    <Listbox value={selected} onChange={setSelected}> 
-      <div className="mt-4 ml-[30%] sm:ml-[45%]">
-        <Listbox.Button className="relative  w-[10rem] cursor-default rounded-lg bg-[#203359] hover:bg-[#517373] py-2 pr-5 shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-            <span className="text-[#94A3B8] text-sm block truncate">{selected.name}</span>
-            <span className=" pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-              <BsChevronContract
-                className="h-5 w-5 text-[#94A3B8]"
-                aria-hidden="true"
-              />
-            </span>
-        </Listbox.Button>
-        <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Listbox.Options className="absolute mt-1 max-h-60 w-10rem overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-              {idioma.map((idioma, idiomaIdx) => (
-                <Listbox.Option
-                  key={idiomaIdx}
-                  className={({ active }) =>
-                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                      active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
-                    }`
-                  }
-                  value={idioma}
-                >
-                  {({ selected }) => (
-                    <>
-                      <span
-                        className={`block truncate ${
-                          selected ? 'font-medium' : 'font-normal'
-                        }`}
-                      >
-                        {idioma.name}
-                      </span>
-                      {selected ? (
-                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                          <BsCheckLg className="h-5 w-5" aria-hidden="true" />
-                        </span>
-                      ) : null}
-                    </>
-                  )}
-                </Listbox.Option>
-              ))}
-            </Listbox.Options>
-        </Transition>
-      </div>
-    </Listbox>
     
-    </div>
+      <div className='flex justify-between items-center pt-6 px-6'>      
+        <h1 className={montserrat.className}><Link href='/'>Deleste.Shop</Link></h1>
+        <Box sx={{ minWidth: 80 }} >
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label" className='text-white '>Idioma</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="Age"
+              onChange={handleChange}
+              className='text-white'
+            >
+              <MenuItem value={'/'} >{<BR className='w-6 h-6'/>}</MenuItem>
+              <MenuItem value={'/arg'}>{<AR className='w-6 h-6'/>}</MenuItem>
+              
+            </Select>
+          </FormControl>
+        </Box>
+        <GoThreeBars className='cursor-pointer text-5xl rounded-lg py-2 px-3 shadow-md hover:bg-[#517373]'/>
+        
+      </div>      
+    
   )
 }
 
