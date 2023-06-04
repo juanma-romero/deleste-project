28bd558f23ae059bb05e76134083aea57e70de4e
Juanma Romero <xjuanmax@hotmail.com>
2023-06-04 14:01:45 -0400

    hice lio con git

--- src/app/br/mapa/mapaGoogle.tsx
+++ src/app/br/mapa/mapaGoogle.tsx
@@ -5,7 +5,7 @@ import { GoogleMap, LoadScriptNext, Marker } from '@react-google-maps/api'
 const Map: React.FC = () => {
   
   return (
-    <LoadScriptNext googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}>
+    <LoadScriptNext googleMapsApiKey={`${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`}>
       <GoogleMap
         mapContainerClassName='w-100 h-96 rounded-xl mx-auto mt-12  '
         mapTypeId='satellite'
