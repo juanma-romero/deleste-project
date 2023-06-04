cb3e131d7e05d2956f7a853e5966506edde47f39
Juanma Romero <xjuanmax@hotmail.com>
2023-06-04 12:00:12 -0400

    tratando arreglar error en vercel

--- src/app/br/mapa/mapaGoogle.tsx
+++ src/app/br/mapa/mapaGoogle.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
+import { GoogleMap, LoadScriptNext, Marker } from '@react-google-maps/api'
 
 const Map: React.FC = () => {
   
   return (
-    <LoadScript googleMapsApiKey={`${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`}>
+    <LoadScriptNext googleMapsApiKey={`${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`}>
       <GoogleMap
         mapContainerClassName='w-100 h-96 rounded-xl mx-auto mt-12  '
         mapTypeId='satellite'
@@ -14,7 +14,7 @@ const Map: React.FC = () => {
       >
         <Marker position={{ lat: -25.510827, lng: -54.611091 }} />
       </GoogleMap>
-    </LoadScript>
+    </LoadScriptNext>
   );
 };
 
